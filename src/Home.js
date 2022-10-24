6f64875cc2ca906c0818938449d324e5314afdc0
Sohaib <sohaibakhtar0031@gmail.com>
2022-10-24 08:33:23 +0100

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome!", body: "lorem ipsum...", author: "mario", id: 2 },
+    { title: "MY Top Tips", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button>Click Me</button>
+      <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 };
